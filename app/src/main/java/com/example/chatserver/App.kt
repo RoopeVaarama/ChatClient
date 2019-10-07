138a90ea410413409cedb1b42532559655960122
RoopeVaarama <roope.vaarama@gmail.com>
2019-10-07 22:20:25 +0300

    Commit

--- app/src/main/java/com/example/chatserver/App.kt
+++ app/src/main/java/com/example/chatserver/App.kt
@@ -2,6 +2,7 @@ package com.example.chatserver
 
 import android.app.Application
 
+//Class for storing the users name
 class App:Application() {
     companion object
     lateinit {
--- app/src/main/java/com/example/chatserver/MainActivity.kt
+++ app/src/main/java/com/example/chatserver/MainActivity.kt
@@ -4,52 +4,70 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.CompoundButton
-import android.widget.Switch
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
-import kotlinx.android.synthetic.main.activity_main.*
-import java.net.ServerSocket
-import java.net.Socket
-import java.net.SocketAddress
-
 class MainActivity : AppCompatActivity() {
 
-
+    var checked = false
     private var username: String = " "
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
-        val switch = findViewById<Switch>(R.id.modeSwitch)
-        switch.setOnClickListener(View.OnClickListener {
-            if(switch.isChecked){
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    }
 
+   override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.mymenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+            return when (item.itemId) {
+                R.id.app_bar_switch -> {
+                    nightMode()
+                    return true
+                }
+                R.id.menuitem -> {
+
+                    return true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+
+    fun nightMode() {
+        runOnUiThread {
+            checked = if (!checked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                true
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-        })
-
-    }
-        fun login(view: View) {
-            val inputField = findViewById<TextView>(R.id.nameView)
-            username = inputField.text.toString()
-            if (username.isEmpty()) {
-                Toast.makeText(
-                    applicationContext,
-                    "Username should not be empty",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                App.user = username
-                Log.d("asd", username)
-                val chat = Intent(this, ChatActivity::class.java)
-                Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_SHORT).show()
-                startActivity(chat)
+                false
             }
         }
     }
+    fun login(view: View) {
+        val inputField = findViewById<TextView>(R.id.nameView)
+        username = inputField.text.toString()
+        if (username.isEmpty()) {
+            Toast.makeText(
+                applicationContext,
+                "Username should not be empty",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            App.user = username
+            Log.d("asd", username)
+            val chat = Intent(this, ChatActivity::class.java)
+            Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_SHORT).show()
+            startActivity(chat)
+        }
+    }
+}
